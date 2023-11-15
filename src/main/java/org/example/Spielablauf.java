3ef099efeb759615722e47e22cfabf66dc55f5e1
Tobias <tibistruppi.te@gmail.com>
2023-11-15 22:21:57 +0100

    Spielablauf fertiggestellt / für acht Stiche ausgelegt

--- src/main/java/org/example/Spielablauf.java
+++ src/main/java/org/example/Spielablauf.java
@@ -10,7 +10,9 @@ public class Spielablauf {
     private List<Karte> gespielteKarten, eigeneKarten, tischKarten, unbekannteKarten;
     private SpielController spiel;
     private int startSpieler;
-    private boolean aktiverSpieler;
+    private boolean aktiverSpieler; //ob der bot Spieler ist oder nicht
+
+    private int gemachteStiche;
 
 
     Spielablauf(SpielController spiel, List<Karte> hand, int startSpieler, boolean aktiverSpieler) {
@@ -21,31 +23,44 @@ public class Spielablauf {
         List<Karte> alleKarten = KartenUtil.initializeSchafKopfCardDeck();
         this.unbekannteKarten=new ArrayList<>(alleKarten);
         this.unbekannteKarten.removeAll(this.eigeneKarten);
-        einStich(startSpieler);
+        gespielteKarten=new ArrayList<>();
+        tischKarten=new ArrayList<>();
+        gemachteStiche=0;
+        einStich();
     }
 
-    public void einStich(int startSpieler){
-        int i=startSpieler;
-        while(i%4!=0){
-            Karte gelegteKarte = null; //warten auf Karteneingabe
-            tischKarten.add(gelegteKarte);
-            unbekannteKarten.remove(gelegteKarte);
-            i++;
+    public void einStich(){
+        while(gemachteStiche<8){
+            int i = startSpieler;
+            while (i % 4 != 0) {
+                Karte gelegteKarte = null; //warten auf Karteneingabe
+                tischKarten.add(gelegteKarte);
+                unbekannteKarten.remove(gelegteKarte);
+                i++;
+            }
+
+            //eigeneKarteLegen
+            Karte eigeneGelegteKarte = spiel.welcheKarteSpielIch(gespielteKarten, eigeneKarten, tischKarten, unbekannteKarten, aktiverSpieler); //Welche KArte soll der Bot spielen, Achtung! unterschiedliche Länge der Listen pro Anfrage
+            tischKarten.add(eigeneGelegteKarte);
+            eigeneKarten.remove(eigeneGelegteKarte);
+
+            while (tischKarten.size() < 4) { //tischkarten zählen
+                Karte gelegteKarte = null; //warten auf Karteneingabe
+                tischKarten.add(gelegteKarte);
+                unbekannteKarten.remove(gelegteKarte);
+            }
+
+            int stichSpieler = spiel.werHatGestochen(tischKarten); //returns int(0,1,2,3)
+            startSpieler = startSpieler + stichSpieler;
+            if (startSpieler > 3) {
+                startSpieler = startSpieler % 4;
+            }
+
+            gespielteKarten.addAll(tischKarten);
+            tischKarten.clear();
+
+            gemachteStiche++;
         }
-
-        //eigeneKarteLegen
-        Karte eigeneGelegteKarte = spiel.welcheKarteSpielIch(gespielteKarten, eigeneKarten, tischKarten, unbekannteKarten); //Welche KArte soll der Bot spielen
-        tischKarten.add(eigeneGelegteKarte);
-        eigeneKarten.remove(eigeneGelegteKarte);
-
-        while (tischKarten.size()<4){ //tischkarten zählen
-            Karte gelegteKarte = null; //warten auf Karteneingabe
-            tischKarten.add(gelegteKarte);
-            unbekannteKarten.remove(gelegteKarte);
-        }
-
-        gespielteKarten.add(null);
-
     }
 
 
