09c38c81dd8f024150b2c5b4d97232a998f64754
Valentin Heiserer <73257760+Vale54321@users.noreply.github.com>
2024-04-16 22:24:11 +0200

    Mono Repo
    * moveBackend
    
    * added Frontend
    
    * added env support for COM port
    
    * added frontend into monorepo

---
+++ Frontend/src/components/CardComponent.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import {onBeforeMount, ref, watch} from 'vue';
+import {Card} from "../BackendMessage";
+
+const props = defineProps<{
+  card: Card;
+}>();
+
+const imgSrc = ref('/assets/card_back.png')
+
+const focus = ref(false);
+
+onBeforeMount(() => {
+  imgSrc.value = "/assets/" + props.card.toString().toLowerCase() + ".png"
+})
+
+watch(() => props.card, (newCard) => {
+  imgSrc.value = `/assets/${newCard.toString().toLowerCase()}.png`;
+});
+</script>
+<template>
+  <div
+      :class="{'!scale-105 !z-10 !top-1/2 !left-1/2' : focus}" class="card transition  overflow-hidden"
+      @click="focus=!focus">
+    <img class="h-full rounded-[1rem] mx-auto" :src="imgSrc" alt="card">
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  &.xl {
+    @apply h-[48rem];
+    img {
+      @apply rounded-[2rem]
+    }
+  }
+
+  &.md {
+    @apply h-96;
+    img {
+      @apply rounded-2xl
+    }
+  }
+
+  &.sm {
+    @apply h-64;
+    img {
+      @apply rounded-xl
+    }
+  }
+}
+</style>
