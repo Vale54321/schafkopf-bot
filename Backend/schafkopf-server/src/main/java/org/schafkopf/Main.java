dc1c5205603b62d8c42527ebd03de7f996de511c
Valentin Heiserer <valentin@heiserer.de>
2024-04-18 00:24:55 +0200

    fixed building issues

--- Backend/schafkopf-server/src/main/java/org/schafkopf/Main.java
+++ Backend/schafkopf-server/src/main/java/org/schafkopf/Main.java
@@ -8,11 +8,9 @@ import org.schafkopf.karte.KartenUtil;
 public class Main {
   /** Creates an Instance of the Backend Server. */
   public static void main(String[] args) {
-    // Press Alt+Eingabe with your caret at the highlighted text to see how
-    // IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+
+    System.out.println("Hello and welcome!");
     KartenListe testHand = KartenUtil.zieheZufallsHand(8);
-    // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
     for (Karte karte : testHand.getKartenListe()) {
       System.out.println(karte.getName());
     }
