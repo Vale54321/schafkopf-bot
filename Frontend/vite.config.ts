033f30f2a665c0154a053076da91dbf5f9f12c89
Valentin Heiserer <73257760+Vale54321@users.noreply.github.com>
2024-04-23 22:15:11 +0200

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,6 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [vue()],
     build: {
-        outDir: '../Backend/src/main/resources/web-content',
+        outDir: '../Backend/schafkopf-client/src/main/resources/web-content',
     },
 })
